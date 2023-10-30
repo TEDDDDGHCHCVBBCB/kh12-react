fc3f2540aa3df3215030171ac69097d8b6e4c38c
TEDDDDGHCHCVBBCB <akdrjsdl102@naver.com>
2023-10-30 18:20:26 +0900

    과제

--- demo03/src/App.js
+++ demo03/src/App.js
@@ -2,15 +2,21 @@
 import './App.css';
 import Exam01 from './components/Exam01';
 import Exam02 from './components/Exam02';
+import Exam03 from './components/Exam03';
+import Exam04 from './components/Exam04';
 
 function App() {
   return (
     <>
-    <h1>각종 리액트 예제</h1>
+    <h1>각종 리액트 예제</h1> 
     <hr/>
     <Exam01/>
     <hr/>
     <Exam02/>
+    <hr/>
+    <Exam03/>
+    <hr/>
+    <Exam04/>
     </>
   );
 }
---
+++ demo03/src/components/Exam04.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+
+//function Exam03(){};;
+//const Exam3 = function(){};
+function Exam03() {
+    const [money, setMoney] = useState(0);
+
+    return (
+        <>
+            <h1>네 번째 예제</h1>
+            <h2>(Q) 주말에 뭐하세요? </h2>
+            <br />
+            <textarea>
+
+            </textarea>
+            <br />
+
+        </>
+    );
+}
+
+export default Exam03;
